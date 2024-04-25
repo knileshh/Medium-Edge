065102e23a43aa2ccffcc07bac12c6e217de578d
knilesh9 <neutron_stays@protonmail.com>
2024-04-25 22:29:58 +0530

    Fix jwt and verify and some little jwt secrets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,29 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
+app.use('api/v1/blog/*', async (c, next) => {
+  const header = c.req.header("authorization") || "";
+
+  const token = header.split(" ")[1]
+
+  const response = await verify(token, c.env.JWT_SECRET)
+
+  if(response.id){
+    next()
+  } else {
+    c.status(403)
+    return c.json({ error: "unauthorized" })
+  }
+})
 
 app.get('/api/v1/blog/:id', (c) => { 
 
@@ -30,15 +45,35 @@ app.post('/api/v1/signup',async (c) => {
     },
   })
 
-  const token = sign({ id: user.id }, "secret")
+  const token = sign({ id: user.id }, c.env.JWT_SECRET)
 
   return c.json({
     jwt: token
   })
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signin', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL ,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  });
+
+  if(!user){
+    c.status(403);
+    return c.json({error: "user not found"});
+  }
+
+  const jwt = await sign({ id: user.id }, e.env.JWT_SECRET);
+
+  return c.json({ jwt });
 })
 
 app.post('/api/v1/blog', (c) => {
