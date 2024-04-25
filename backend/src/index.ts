2f4ef3f07a8cc0b0320ae67878770fae456351c2
knilesh9 <neutron_stays@protonmail.com>
2024-04-25 21:49:15 +0530

    Prisma Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,33 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
-app.get('/api/v1/blog/:id', (c) => {
+app.get('/api/v1/blog/:id', (c) => { 
+
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signup',async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  })
+
   return c.text('Hello Hono!')
 })
 
